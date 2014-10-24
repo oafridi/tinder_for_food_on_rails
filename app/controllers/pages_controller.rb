3a9babede7cf91ac2505153ed99c37e5910051ee
Andrew Archer <archer@archer-macbookpro3.roam.corp.google.com>
2014-10-24 16:02:22 -0700

    Restaurant displays

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,28 +14,27 @@ class PagesController < ApplicationController
   def liked_items
     @user = User.last
     @rated_item_ids = Rating.where("user_id = ? AND liked = ?", @user.id, true)
-    @item_ids = []
-    @rated_item_ids.each { |rated| @item_ids << rated.item_id }
+    @items = []
 
-    if !@item_ids || @item_ids.empty?
+    @rated_item_ids.each do |rated|
+      item = Item.find(rated.item_id)
+      @items <<
+      {:item  => item,
+       :restaurant => item.restaurant }
+    end
+
+    if !@items || @items.empty?
       render "none_liked"
     end
   end
 
   def view
     @user = User.last
-    # puts "UserId:"
-    # p @user
     rated_item_ids = Rating.where('user_id = ?',@user.id).pluck(:item_id)
-    puts "Rated Item Ids:"
-    # p rated_item_ids
     item_ids = Item.all.pluck(:id)
     unrated_item_ids = item_ids.reject do |item_id|
       rated_item_ids.include? item_id
     end
-    puts "Unrated Item Ids:"
-    p unrated_item_ids
-
     begin
       @item = Item.find(unrated_item_ids.sample)
     rescue ActiveRecord::RecordNotFound => e
